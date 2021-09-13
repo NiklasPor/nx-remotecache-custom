ea8853504a255a3c825f420189946b8deb196df2
Niklas Portmann <niklaspor@gmail.com>
2021-09-13 20:03:43 +0200

    format

--- lib/create-remote-cache-retrieve.ts
+++ lib/create-remote-cache-retrieve.ts
@@ -1,11 +1,12 @@
 import { RemoteCache } from "@nrwl/workspace/src/tasks-runner/default-tasks-runner";
 import AdmZip from "adm-zip";
 import { promises } from "fs";
-const { writeFile } = promises;
-
 import { join } from "path";
 import { promisify } from "util";
+
 import { SafeRemoteCacheImplementation } from "./types/safe-remote-cache-implementation";
+
+const { writeFile } = promises;
 
 const COMMIT_FILE_EXTENSION = ".commit";
 const COMMIT_FILE_CONTENT = "true";
